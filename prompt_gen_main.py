a275de3822e2c005d9696ae909e28313fe55c19a
BuckleMashu <cuongnguyenlvl30@gmail.com>
2025-07-20 13:39:12 +0800

    Update prompt_gen_main.py

--- prompt_gen_main.py
+++ prompt_gen_main.py
@@ -12,13 +12,16 @@ import re
 
 import textwrap
 
+# --- 1. Configuration ---
 # CHROMA_DB_PATH = "SML/PADBRC/PADBRC_vector_DB"
 CHROMA_DB_PATH = "SML/courses_vector_DB"
 COLLECTION_NAME = "smartlearn_padbrc"
-EMBEDDING_MODEL_NAME = 'all-MiniLM-L6-v2' # Ensure this is THE SAME model used for ingestion
+EMBEDDING_MODEL_NAME = 'all-MiniLM-L6-v2'
 chosen_model = 'qwen3'
 
 class PG_SML():
+    # --- 2. Initialize ChromaDB Client and Embedding Model ---
+    #CAN BE FOUND IN THE STORE_DATA_TESTING_V1.IPYNB
     @staticmethod
     def initialise_chromadb_and_embedding_model(chromadb_path,embed_model_name):
         try:
@@ -37,10 +40,11 @@ class PG_SML():
         
         return client,embedding_model
     
+    # --- 3. Get the Collection ---
     @staticmethod
     def get_collection(client,collection_name):
         try:
-            collection = client.get_collection(name=collection_name)
+            collection = client.get_collection(name=collection_name) # Use get_collection, not get_or_create
             print(f"Successfully connected to collection '{collection_name}'.")
             print(f"Total items in collection: {collection.count()}")
             if collection.count() == 0:
@@ -52,6 +56,10 @@ class PG_SML():
             exit()
         return collection
     
+
+    # --- 5. Loop Through Questions, Embed, Query, and Display Results ---
+    # k < 10 and 20 and 40, cant name dr mai ngo
+    # k = 100 works, mentions both mai ngo and nghia tran
     @staticmethod
     def embed_question_and_retrieves(instructors,user_question,embedding_model,collection,prompt_type,module_name,num_results_to_fetch = 75,full_response = None):
         try:
@@ -74,6 +82,7 @@ class PG_SML():
         if not retrieved_documents:
             print("No relevant documents found in the database for this question.")
 
+        # --- 6. Construct Prompt for LLM ---
         if retrieved_documents:
             context_for_llm = ""
             for j,doc_text in enumerate(retrieved_documents):
@@ -88,18 +97,20 @@ class PG_SML():
                 ---STUDENT'S CURRENT QUESTION---
                 {user_question}
 
-                You are a teacher for the website "SmartLearnSolution.com".
+                You are a teacher for the website smartlearnsolution.com.au
                 Right now, you will be answering the student's question based on the content from course "{module_name}", which is provided as CONTEXT FOR ANSWERING.
                 The main instructors of this course are {instructors}.
                 Please answer the student question as if you are confident about this knowledge field.
                 Your personality is: friendly, helpful, encouraging, and patient. But not too much, be human-like.
+                Obviously, you will always unable to understand the student's situation clearly, so if it necessary, ask the student one or two question so you can get a clearer idea of the problem they are facing.
 
                 YOUR RULES:
-                1.  Your entire response will be based on the "CONTEXT FOR ANSWERING" as much as possible.
+                1.  Your entire response will purely be based on the "CONTEXT FOR ANSWERING".
                 2.  NEVER mention the "CONTEXT FOR ANSWERING" to the student. "CONTEXT FOR ANSWERING" is simply for you and only you to understand, with "CONTEXT FOR ANSWERING" acting like your brain. Use the information within them to answer the question and DO NOT EVEN REFER ABOUT THE CONTEXT TO THE STUDENT EVER OR YOULL DIE.
                 3.  Answer the student's question. However, since the conversational medium is word, there might not be enough context for you to know the entire problem. So if there is something missing that crucial to solve this, let the student know and ask them appropriate question.
                 4.  If your answer is not "teacher-like", politely say "I'm sorry, I couldn't find specific information on that topic in the course materials."
                 5.  Format your answers for clarity using Markdown. Use lists, bold text, and paragraphs to structure your response.
+                6.  After answering the student, please list where (source_identifier , can be found in the metadata between " ") the student can find the relevant documentation relating to the question. DO NOT MAKE UP FAKE URL, ALWAYS COMPARE WHATEVER URLs YOU ARE PROVIDING THE STUDENTS WITH THE ONES YOU RECEIVED IN THE METADATA. IF IT DOES NOT MATCH, DO NOT SEND IT TO THE STUDENT.
 
                 From this point onward, whatever prompt you receives will be straight from the student.
                 """
@@ -115,19 +126,21 @@ class PG_SML():
                 ---CONVERSATION HISTORY--- This is the conversation so far (you dont need to answers the questions found in here. It is for you to reference)
                 {full_response}
 
-                You are a teacher for the website "SmartLearnSolution.com".
+                You are a teacher for the website smartlearnsolution.com.au 
                 Right now, you will be answering the student's question based on the content from course "{module_name}", which is provided as CONTEXT FOR ANSWERING.
                 The main instructors of this course are {instructors}.
                 Please answer the student question as if you are confident about this knowledge field.
                 Your personality is: friendly, helpful, encouraging, and patient. But not too much, be human-like.
+                Obviously, you will always unable to understand the student's situation clearly, so if it necessary, ask the student one or two question so you can get a clearer idea of the problem they are facing.
 
                 YOUR RULES:
-                1.  Your entire response will be based on the "CONTEXT FOR ANSWERING" as much as possible.
+                1.  Your entire response will be purely based on the "CONTEXT FOR ANSWERING".
                 2.  NEVER mention the "CONTEXT FOR ANSWERING" to the student. "CONTEXT FOR ANSWERING" is simply for you and only you to understand, with "CONTEXT FOR ANSWERING" acting like your brain. Use the information within them to answer the question and DO NOT EVEN REFER ABOUT THE CONTEXT TO THE STUDENT EVER OR YOULL DIE.
                 3.  Answer the student's question. However, since the conversational medium is word, there might not be enough context for you to know the entire problem. So if there is something missing that crucial to solve this, let the student know and ask them appropriate question.
                 4.  If your answer is not "teacher-like", politely say "I'm sorry, I couldn't find specific information on that topic in the course materials."
                 5.  Format your answers for clarity using Markdown. Use lists, bold text, and paragraphs to structure your response.
-                6.  Refer to the "CONVERSATION HISTORY" to understand the flow of the conversation and avoid repeating information.
+                6.  Refer to the "CONVERSATION HISTORY" to understand the flow of the conversation and avoid repeating information. DO NOT ANSWER QUESTIONS MARKED WITH ---The student's past question:
+                7.  After answering the student, please list where (source_identifier , can be found in the metadata between " ") the student can find the relevant documentation relating to the question. DO NOT MAKE UP FAKE URL, ALWAYS COMPARE WHATEVER URLs YOU ARE PROVIDING THE STUDENTS WITH THE ONES YOU RECEIVED IN THE METADATA. IF IT DOES NOT MATCH, DO NOT SEND IT TO THE STUDENT.
 
                 From this point onward, whatever prompt you receives will be straight from the student.
                 """
@@ -161,6 +174,10 @@ class PG_SML():
         
         return prompt_to_llm
 
+    #type-->init = initial. Setting up the major context for the LLM
+    #        cont = continue. No need to repeat the setting context, for the continuation of the conversation
+
+    # --- 7. Generate Answers -------
     @staticmethod
     def generate_answer(user_question,chosen_model,prompt_type,prompt_to_llm = None,full_response_past = None):
         print("--- LLM Response ---")
@@ -216,13 +233,16 @@ class PG_SML():
                     if chunk['message']['content'] == '</think>':
                         out_of_think = True
                     
+                    # full_response += chunk['message']['content']
         except Exception as e:
             print(f"Error with streaming chat API: {e}")
         
+        #thinking is removed for clarity and token efficiency
         answer = re.sub(r"<think>.*?</think>", "", answer, flags=re.DOTALL)
-        full_response += f"---The student's question:{user_question}---"
+        full_response += f"---The student's past question:{user_question}---"
         full_response += answer
         return full_response, answer
+    #type-->summ = summarise, to save token space
 
     @staticmethod
     def get_string_token_count(full_response):
